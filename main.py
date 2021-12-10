79fe810d61769c39083b253de28686c9de742766
SilkySantosh <73717776+silky09@users.noreply.github.com>
2021-12-10 10:28:35 +0000

    add new folders and kept all files inside it.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # import Casting
 # import User_Input
 # import Indentation
-# import Variables
 #import Data_Types
+from Variables import Variables
 #from Strings import Strings
 #from Strings import Slicing_String
 #from Strings import Modify_String
 #from Strings import EscapeCharacter
 #import Numbers
-from Booleans import Booleans
+#from Booleans import Booleans
 
 #print Hello World!
 print()
